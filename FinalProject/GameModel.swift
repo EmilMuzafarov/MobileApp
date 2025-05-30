2a8ccd253464bab0d7b2fa1067769a212d2ce74e
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2025-05-30 09:14:05 -0400

    Update GameModel.swift

--- FinalProject/GameModel.swift
+++ FinalProject/GameModel.swift
@@ -12,4 +12,16 @@ class GameModel: ObservableObject {
     @Published var actorList: [GameActor] = []
     @Published var player: GameActor = GameActor()
     @Published var taskList: [GameTask] = []
+    var row:Int = 10
+    var column:Int = 7
+    @Published var buildingGrid: [[TileView]] = [[]]
+    func build() {
+        for _ in 1...row {
+            for _ in 1...column {
+                self.buildingGrid[row][column] = TileView(tile: Tile(tileType: TileType.STAIRS))
+                column+=1
+            }
+            row+=1
+        }
+    }
 }
