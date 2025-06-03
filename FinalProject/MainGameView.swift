3725058ac8f4bf673358933679f95ef70cc77c84
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2025-06-03 09:06:59 -0400

    Fix task button navigation

--- FinalProject/MainGameView.swift
+++ FinalProject/MainGameView.swift
@@ -6,51 +6,39 @@
 //
 
 import SwiftUI
-
+let tasks: [String] = ["Homework", "Attendance"]
+var completed = 0
 struct MainGameView: View {
+    @State private var showTask = false
     var body: some View {
-        VStack {
-            Button() {
-                //TaskView()
-            } label: {
+        NavigationStack {
+            VStack {
                 GameControlButton(buttonType: ButtonConstants.ButtonType.TO_DO_BUTTON) {
-                    
+                    showTask = true
                 }
-                    
+                .offset(x:150, y:-50)
+                
+                GameView()
+                HStack {
+                    HStack(spacing: 0.0) {
+                        GameControlButton(buttonType: ButtonConstants.ButtonType.MOVE_PLAYER_LEFT) {
+                            
+                        }
+                        GameControlButton(buttonType: ButtonConstants.ButtonType.MOVE_PLAYER_RIGHT) {
+                            
+                        }
+                    }
+                    Spacer()
+                    GameControlButton(buttonType: ButtonConstants.ButtonType.PLAYER_INTERACT) {
+                        
+                    }
+                }
+                .padding(0)
             }
-            .offset(x:150, y:-50)
-            GameView()
-            HStack {
-                HStack(spacing: 0.0) {
-                    GameControlButton(buttonType: ButtonConstants.ButtonType.MOVE_PLAYER_LEFT) {
-                        
-                    }
-                    GameControlButton(buttonType: ButtonConstants.ButtonType.MOVE_PLAYER_RIGHT) {
-                        
-                    }
-//                    Button() {
-//                        print("LEFT")
-//                    } label: {
-//                        Image("directionButton")
-//                            .interpolation(.none)
-//                            .resizable()
-//                            .frame(width: 50, height: 50)
-//                    }
-//                    Button() {
-//                        print("RIGHT")
-//                    } label: {
-//                        Image("directionButton")
-//                            .resizable()
-//                            .frame(width: 50, height: 50)
-//                    }
-                }
-                Spacer()
-                GameControlButton(buttonType: ButtonConstants.ButtonType.PLAYER_INTERACT) {
-                    
-                }
+            .navigationDestination(isPresented: $showTask) {
+                TaskView()
             }
         }
-        .padding(0)
     }
 }
 
